c45e09226112187cc776663dd6c3aeefd19d172f
harsha23-pal <harsha230109@gmail.com>
2026-06-14 10:52:33 +0530

    Py

--- pythonbegin.py
+++ pythonbegin.py
@@ -1,2 +1,4 @@
-Name=str(input("Enter your Name please:"))
-print("Hello",Name,"!")
+name1=str(input("Enter your first name:"))
+name2=str(input("Enter your second name:"))
+Name=name1+" "+name2
+print(Name)
